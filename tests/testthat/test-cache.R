17d6cb1723f4a43955eaec2e7ab5e44e340f2d6e
Kohei Watanabe <watanabe.kohei@gmail.com>
2020-08-01 06:36:24 +0200

    Add repeated tests

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -8,20 +8,30 @@ test_that("test caching is working", {
     dfmt <- dfm(toks)
     
     cache1 <- sample(1:100, 10)
-    quanteda:::set_cache(corp, "something", cache1)
+    for (i in 1:10) {
+        quanteda:::set_cache(corp, "something", cache1)
+    }
     expect_identical(attr(corp, "meta")$object$something$data, cache1)
-    expect_identical(quanteda:::get_cache(corp, "something"), cache1)
-    
+    for (i in 1:10) {
+        expect_identical(quanteda:::get_cache(corp, "something"), cache1)
+    }
     cache2 <- sample(1:100, 10)
-    quanteda:::set_cache(toks, "something", cache2)
+    for (i in 1:10) {
+        quanteda:::set_cache(toks, "something", cache2)
+    }
     expect_identical(attr(toks, "meta")$object$something$data, cache2)
-    expect_identical(quanteda:::get_cache(toks, "something"), cache2)
+    for (i in 1:10) {
+        expect_identical(quanteda:::get_cache(toks, "something"), cache2)
+    }
     
     cache3 <- sample(1:100, 10)
-    quanteda:::set_cache(dfmt, "something", cache3)
+    for (i in 1:10) {
+        quanteda:::set_cache(dfmt, "something", cache3)
+    }
     expect_identical(attr(dfmt, "meta")$object$something$data, cache3)
-    expect_identical(quanteda:::get_cache(dfmt, "something"), cache3)
-    
+    for (i in 1:10) {
+        expect_identical(quanteda:::get_cache(dfmt, "something"), cache3)
+    }
 })
 
 test_that("test caching is passed to downstream objects", {
